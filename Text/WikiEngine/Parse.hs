d61345571eb81a2653c0c87ed9c777b5ae492fc9
Vincent Hanquez <vincent@snarc.org>
2011-06-03 08:41:29 +0100

    proper fix for blanks before blocks

--- Text/WikiEngine/Parse.hs
+++ Text/WikiEngine/Parse.hs
@@ -12,7 +12,7 @@ parseDocument :: String -> Either ParseError [Block]
 parseDocument s = parse document "source" (s ++ "\n\n")
 
 document :: P [Block]
-document = many block
+document = many (oneOf "\r\n\t ") >> many block
 
 block :: P Block
 block = choice
